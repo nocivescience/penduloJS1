663473b847ef0524d5d20baf976204def9c90e26
nocive.science <nocive.science@gmail.com>
2024-02-17 09:08:39 -0300

    quedo super

--- script.js
+++ script.js
@@ -1,33 +1,68 @@
-window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+class Pendulum {
+    constructor(centerX, centerY, length, angle, gravity, freq) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.length = length;
+        this.angle = angle;
+        this.gravity = gravity;
+        this.time = 0;
+        this.timeIncrement = 0.1;
+        this.freq = freq;
+    }
 
-    var length = 200;
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var gravity = 9.8; // Aceleración debido a la gravedad
-    var time = 0;
-    var timeIncrement = 0.1;
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Calcular el ángulo usando la fórmula del movimiento armónico simple
-        var angle = Math.PI / 4 * Math.cos(Math.sqrt(gravity / length) * time);
-
-        var x = centerX + length * Math.sin(angle);
-        var y = centerY + length * Math.cos(angle);
+    draw(ctx) {
+        var x = this.centerX + this.length * Math.sin(this.angle);
+        var y = this.centerY + this.length * Math.cos(this.angle);
 
         ctx.beginPath();
-        ctx.moveTo(centerX, centerY);
+        ctx.moveTo(this.centerX, this.centerY);
         ctx.lineTo(x, y);
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.arc(x, y, 20, 0, 2 * Math.PI);
+        ctx.arc(x, y, 2, 0, 2 * Math.PI);
         ctx.fill();
 
-        time += timeIncrement;
+        this.time += this.timeIncrement;
+        this.angle = Math.PI / 4 * Math.cos(Math.sqrt(this.gravity / this.length*this.freq) * this.time);
+    }
+}
+
+function linspace(start, end, n) {
+    var step = (end - start) / (n - 1);
+    return Array.from({ length: n }, (d, i) => start + i * step);
+}
+let pendulums = [];
+window.onload = function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    const arrayFreq = linspace(.1, .05, 150);
+    const arrayLength = linspace(50, 530, 150);
+
+    if (arrayFreq.length === arrayLength.length) {
+        for (let i = 0; i < arrayFreq.length; i++) {
+            pendulums.push(new Pendulum(canvas.width / 2, canvas.height / 2-250, arrayLength[i], 0, 9.8, arrayFreq[i]));
+        }
+    } else {
+        console.log("Los arrays no tienen la misma longitud");
+    }
+
+    // for (let i in arrayFreq) {
+    //     for (let j in arrayLength) {
+    //         pendulums.push(new Pendulum(canvas.width / 2, canvas.height / 2, arrayLength[j], 0, 9.8, arrayFreq[i]));
+    //     }
+    // }
+
+    // var pendulums = [
+    //     new Pendulum(canvas.width / 2, canvas.height / 2, 200, 0, 9.8),
+    //     new Pendulum(canvas.width / 2, canvas.height / 2, 150, 0, 9.8),
+    //     new Pendulum(2 * canvas.width / 2, canvas.height / 2, 250, 0, 9.8)
+    // ];
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        pendulums.forEach(pendulum => pendulum.draw(ctx));
     }
 
     setInterval(draw, 10);
